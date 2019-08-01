114fa1c762b6998eae299ee1415455d1b4a0d357
Jeremy Green <jeremy@octolabs.com>
2019-07-31 21:10:27 -0500

    this happened for some reason

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171004015121) do
+ActiveRecord::Schema.define(version: 2017_10_04_015121) do
 
   create_table "widgets", force: :cascade do |t|
     t.string "title"
