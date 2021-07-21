86afb354bed81434a5b7a476cd4ad458dc3cafd1
Jeremy Green <jeremy@octolabs.com>
2021-07-21 13:50:18 -0500

    Fix a require problem

--- lib/polaris_view_helpers.rb
+++ lib/polaris_view_helpers.rb
@@ -1,8 +1,9 @@
 require 'polaris_view_helpers/helper'
+require 'action_view'
 
 module PolarisViewHelpers
   ActionView::Base.send :include, PolarisViewHelpers::Helper
 
-  class Engine < Rails::Engine
-  end
+  #class Engine < Rails::Engine
+  #end
 end
