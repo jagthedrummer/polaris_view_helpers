f5a0fe361a50a2fe329eb6c6ac7e3d0fd7b2095e
Jeremy Green <jeremy@octolabs.com>
2023-05-25 15:15:39 -0500

    try to get heroku working

--- test/dummy/app/models/widget.rb
+++ test/dummy/app/models/widget.rb
@@ -1,3 +1,7 @@
-class Widget < ApplicationRecord
+class Widget
+  include ActiveModel::API
+  #include ActiveModel::AttributeMethods
+  attr_accessor :title
+  #define_attribute_methods :title
   validates :title, presence: true
 end
